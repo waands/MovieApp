9e5b3b3eb0a569d80658258706013f3d02a96c29
Wanderson Teixeira <wanderjunior36@gmail.com>
2024-06-04 23:32:57 -0300

    searchbar film poster + fixedfocus when clicking to search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -31,22 +31,26 @@ const SearchBar = () => {
       }
     };
 
+    if (isClicked) {
+      inputRef.current.focus();
+    }
+
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [isClicked]);
 
   const handleSearchIconClick = () => {
     setIsClicked(true);
-    inputRef.current?.focus();
+
   };
 
   return (
-    <div ref={containerRef} className="relative inline-block">
+    <div ref={containerRef} className="relative inline-block w-72">
       {!isClicked ? (
         <button
-          className="pt-1 transition-all duration-300"
+          className="pt-1 transition-all text-gray-500 duration-300 ml-64 hover:text-white"
           onClick={handleSearchIconClick}
         >
           <IoSearch size={20} />
@@ -59,13 +63,24 @@ const SearchBar = () => {
             placeholder="Search"
             onChange={handleInputChange}
             ref={inputRef}
-            className="bg-gray-300 text-black px-2 py-1 rounded-full focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 transition-all duration-300"
+            className="bg-gray-300 text-black px-2 py-1 rounded-full focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-opacity-50 transition-all duration-300 w-72"
           />
           {error && <p className="text-red-500">{error.message}</p>}
           <ul className="absolute left-0 right-0 z-50 mt-2 bg-gray-800">
             {movies.map((movie) => (
               <Link to={`/movie/${movie.id}`} key={movie.id}>
-                <li className="p-2 hover:bg-secondary text-white">{movie.title}</li>
+                <li className="p-2 hover:bg-secondary text-white text-sm">
+                  <div className="flex inline-flex">
+                    {movie.poster_path && (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
+                        alt={movie.title}
+                        className="w-15 h-20 mr-2 items-center"
+                      />
+                    )}
+                    {movie.title}
+                  </div>
+                </li>
               </Link>
             ))}
           </ul>
